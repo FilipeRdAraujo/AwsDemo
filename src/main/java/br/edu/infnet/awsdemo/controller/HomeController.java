b57e48ebb44c63e2109d3e6d5658faf2eb1ebcef
FilipeRdAraujo <60610532+FilipeRdAraujo@users.noreply.github.com>
2021-06-01 10:16:08 -0300

    .

--- src/main/java/br/edu/infnet/awsdemo/controller/HomeController.java
+++ src/main/java/br/edu/infnet/awsdemo/controller/HomeController.java
@@ -3,7 +3,6 @@ package br.edu.infnet.awsdemo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.edu.infnet.awsdemo.model.AmazonClient;
